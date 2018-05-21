5d7ca768a0a9513753866aa55eb296a145034fd3
farooqam <farooq_am@hotmail.com>
2018-05-21 01:15:31 -0700

    Palindrome, reverse string.

---
+++ Csds/CsdsLib.Tests/StringExtensionsTests.cs
@@ -0,0 +1,32 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace CsdsLib.Tests
+{
+    public class StringExtensionsTests
+    {
+        [Theory]
+        [InlineData("foo", "oof")]
+        [InlineData("", null)]
+        [InlineData(" ", null)]
+        [InlineData("bubba gump", "pmug abbub")]
+        [InlineData("A man, a plan, a canal: Panama.", ".amanaP :lanac a ,nalp a ,nam A")]
+        public void Reverse_Reverses_String(string original, string reversed)
+        {
+            original.Reverse().Should().Be(reversed);
+        }
+
+        [Theory]
+        [InlineData("", false)]
+        [InlineData(" ", false)]
+        [InlineData("a", true)]
+        [InlineData("foof foof.", true)]
+        [InlineData("bubba gump", true)]
+        [InlineData("A man, a plan, a cat, a ham, a yak, a yam, a hat, a canal-Panama!", true)]
+
+        public void IsPalindrome_Tests(string s, bool isPalindrome)
+        {
+            s.IsPalindrome().Should().Be(isPalindrome);
+        }
+    }
+}
---
+++ Csds/CsdsLib/StringExtensions.cs
@@ -0,0 +1,65 @@
+﻿using System.Linq;
+
+namespace CsdsLib
+{
+    public static class StringExtensions
+    {
+        public static string Reverse(this string s)
+        {
+            if (string.IsNullOrWhiteSpace(s))
+            {
+                return null;
+            }
+
+            var arr = s.ToCharArray();
+            var head = 0;
+            var tail = arr.Length - 1;
+
+            while (head < tail)
+            {
+                var save = arr[head];
+                arr[head] = arr[tail];
+                arr[tail] = save;
+                head++;
+                tail--;
+            }
+
+            return new string(arr);
+        }
+
+        public static bool IsPalindrome(this string s)
+        {
+            if (string.IsNullOrWhiteSpace(s))
+            {
+                return false;
+            }
+
+            if (s.Length == 1)
+            {
+                return true;
+            }
+
+            var arr = s.ToLowerInvariant()
+                .ToCharArray()
+                .Where(char.IsLetterOrDigit)
+                .ToArray();
+
+
+            var head = 0;
+            var tail = arr.Length - 1;
+
+            while (head < tail)
+            {
+                if (arr[head] != arr[tail])
+                {
+                    return false;
+                }
+
+                head++;
+                tail--;
+            }
+
+            return true;
+        }
+    }
+}
