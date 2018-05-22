017a85615c738a64455da0e7d7ebf24cadd6b216
farooqam <farooq_am@hotmail.com>
2018-05-22 10:09:05 -0700

    Fibbonacci.

---
+++ Csds/CsdsLib.Tests/NumericExtensionsTests.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace CsdsLib.Tests
+{
+    public class NumericExtensionsTests
+    {
+        [Theory]
+        [InlineData(0, 0)]
+        [InlineData(1, 1)]
+        [InlineData(2, 1)]
+        [InlineData(3, 2)]
+        [InlineData(4, 3)]
+        [InlineData(5, 5)]
+        [InlineData(6, 8)]
+        [InlineData(7, 13)]
+        [InlineData(8, 21)]
+        [InlineData(9, 34)]
+        [InlineData(10, 55)]
+        public void Fibbonacci_Tests(int n, int expected)
+        {
+            n.Fibbonacci().Should().Be(expected);
+        }
+    }
+}
---
+++ Csds/CsdsLib/NumericExtensions.cs
@@ -0,0 +1,20 @@
+﻿namespace CsdsLib
+{
+    public static class NumericExtensions
+    {
+        public static int Fibbonacci(this int n)
+        {
+            var a = 0;
+            var b = 1;
+
+            for (var i = 0; i < n; i++)
+            {
+                var save = a;
+                a = b;
+                b = b + save;
+            }
+
+            return a;
+        }
+    }
+}
